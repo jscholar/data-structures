c7721025c7b5e6a518f14faab66ed88e8e22216f
Jin Cai <jinruncai@gmail.com>
2020-02-05 12:29:31 -0800

    Implement Queue's enqueue and dequeue methods
    Dequeue will return null when there is nothing at what should be the
    first element in the queue.
    
    Enqueue updates the last element in the queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   someInstance.storage = {};
-  someInstance.first = null;
+  someInstance.first = 0;
   someInstance.last = null;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (this.last === null) {
+      this.last = 0;
+    } else {
+      this.last++;
+    }
+    this.storage[this.last] = value;
   };
 
   someInstance.dequeue = function() {
-
+    if (this.storage[this.first] === undefined) {
+      return null;
+    } else {
+      this.first++;
+      return this.storage[this.first - 1];
+    }
   };
 
   someInstance.size = function() {
