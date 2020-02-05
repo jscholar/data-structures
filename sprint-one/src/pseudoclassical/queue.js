7321ec4756ddb2465a40da5ded4c07681b5bb02d
Jin Cai <jinruncai@gmail.com>
2020-02-05 15:42:36 -0800

    Implemented pseudoclassical instantiation for Queue and Stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.first = 0;
+  this.last = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.last] = value;
+  this.last++;
+  return value;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.first === this.last) {
+    return null;
+  } else {
+    var next = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    return next;
+  }
+},
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.top = 0;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.top] = value;
+  this.top++;
+  return value;
+}
+
+Stack.prototype.pop = function() {
+  if (top === 0) {
+    return null;
+  }
+  this.top--;
+  var result = this.storage[this.top];
+  delete this.storage[this.top];
+  return result;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
 
 
