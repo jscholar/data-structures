c79eebe057e00ef24a524987391aa3506973d3d8
Jin Cai <jinruncai@gmail.com>
2020-02-05 14:48:26 -0800

    Declare fields for Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,7 +2,8 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+  someInstance.top = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
