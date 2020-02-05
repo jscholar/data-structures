10cf0f3adbc7ef18f04439d48ffaaaf32794a090
Jin Cai <jinruncai@gmail.com>
2020-02-05 15:46:45 -0800

    Implemented es6 instantiation for Queue and Stack

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.first = 0;
+    this.last = 0;
+    this.storage = {};
   }
-
 }
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.last] = value;
+  this.last++;
+  return value;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.first === this.last) {
+    return null;
+  } else {
+    var next = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    return next;
+  }
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
